b7451d483d4c120d8e335622c855f7008d9cbaa5
Carlos Toledo <carlombo@hotmail.com>
2024-07-08 22:47:53 -0400

    [ADD] Created method crear_user in services.py

--- main/services.py
+++ main/services.py
@@ -1,3 +1,5 @@
+from main.models import UserProfile, User
+
 def crear_inmueble(*args):
     pass
 
@@ -7,8 +9,19 @@ def editar_inmueble(*args):
 def eliminar_inmueble(inmueble_id):
     pass
 
-def crear_user(*args):
-    pass
+def crear_user(rut:str, first_name:str, last_name:str, email:str, password:str, direccion:str, telefono:str):
+    user = User.objects.create_user(
+        username=rut,
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        password=password
+    )
+    UserProfile.objects.create(
+        direccion=direccion,
+        telefono_personal=telefono,
+        user=user
+    )
 
 def editar_user(*args):
     pass
