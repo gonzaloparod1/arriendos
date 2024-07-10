38cd43b9f35a07613df8757383c0b431d70192c7
Carlos Toledo <carlombo@hotmail.com>
2024-07-09 20:39:18 -0400

    [UPDATE] Updated crear_user method in services.py

--- main/services.py
+++ main/services.py
@@ -1,4 +1,6 @@
-from main.models import UserProfile, User
+from main.models import UserProfile
+from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
 
 def crear_inmueble(*args):
     pass
@@ -9,19 +11,25 @@ def editar_inmueble(*args):
 def eliminar_inmueble(inmueble_id):
     pass
 
-def crear_user(rut:str, first_name:str, last_name:str, email:str, password:str, direccion:str, telefono:str):
-    user = User.objects.create_user(
-        username=rut,
-        first_name=first_name,
-        last_name=last_name,
-        email=email,
-        password=password
-    )
+def crear_user(username:str, first_name:str, last_name:str, email:str, password:str, pass_confirm:str, direccion:str, telefono:str=None) -> list[bool, str]:
+    if password != pass_confirm:
+        return False, 'Las contraseñas no coinciden'
+    try:
+        user = User.objects.create_user(
+            username,
+            email,
+            password,
+            first_name=first_name,
+            last_name=last_name,
+        )
+    except IntegrityError:
+        return False, 'Rut duplicado'
     UserProfile.objects.create(
         direccion=direccion,
         telefono_personal=telefono,
         user=user
     )
+    return True
 
 def editar_user(*args):
     pass
