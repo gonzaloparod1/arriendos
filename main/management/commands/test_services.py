bf94c5aff5049ccc562864354285aae4c98b05f7
Carlos Toledo <carlombo@hotmail.com>
2024-07-09 21:16:11 -0400

    [ADD] Added test file in folder management/commands

---
+++ main/management/commands/test_services.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+from main.services import *
+
+# Se ejecuta usando python manage.py test_client
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        pass
