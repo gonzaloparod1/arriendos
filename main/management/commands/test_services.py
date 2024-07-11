29d5fc6ffa080a92f5994dedeb79af3b245df812
Carlos Toledo <carlombo@hotmail.com>
2024-07-10 20:50:54 -0400

    [ADD] Created function to load Comunas y Regiones from csv file

--- main/management/commands/test_services.py
+++ main/management/commands/test_services.py
@@ -5,9 +5,9 @@ from main.services import *
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        #crear_user('44.444.444-4', 'Cuatro', 'Jor', 'ccc@bbb.ccc', '123456', '123456', 'Pasaje Nueve, Chile')
+        crear_user('11.111.111-1', 'Pedro', 'Picapiedra', 'aaa@bbb.ccc', '123456', '123456', 'Pasaje Nueve, Chile')
         #editar_user('44.444.444-4', 'Cuatro', 'Jordan', 'ccc@bbb.ccc', '654321', '654321', 'Street One, Usa', '987654321')
         #crear_inmueble('Casa Grande blanca en Villa Alemaan', 'Esta es la descriocion de propiedad', 90, 180, 3, 3, 2, 'Calle 22', 450000, 'casa', 'Villa Alemana', '22.2222.222-2')
         #eliminar_inmueble(1)
-        editar_inmueble(3, 'Parcela Amarilla en Villa Alemana', 'Hermosa parcela amarilla en la esquina', 90, 180, 3, 3, 2, 'Calle 22', 750000, 'parcela', 'Villa Alemana', '22.2222.222-2')
+        #editar_inmueble(3, 'Parcela Amarilla en Villa Alemana', 'Hermosa parcela amarilla en la esquina', 90, 180, 3, 3, 2, 'Calle 22', 750000, 'parcela', 'Villa Alemana', '22.2222.222-2')
         return 'Resultó'
